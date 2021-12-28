d92199996f37a2451d6e7d2f7fded89faa5eba1c
phunghoang3b <71882004+phunghoang3b@users.noreply.github.com>
2021-12-29 00:37:11 +0700

    khai - làm nội dung trang bàn cờ
    Hoàn thành

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -23,7 +23,7 @@
             <div class="col-12 col-sm-6 col-md-4">
                 <div class="single_catagory_area d-flex align-items-center justify-content-center bg-img" style="background-image: url(assets/img/bg-img/buttonchess.jpg);border: 5px solid orange;">
                     <div class="catagory-content">
-                        <a href="#">ĐĂNG NHẬP</a>
+                        <a href="">ĐĂNG NHẬP</a>
                     </div>
                 </div>
             </div>
